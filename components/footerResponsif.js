3e459c19f8e6b9664dc344b7a50d7ca5dc8d693f
Kolottioloman Kone <kolotioloman.kone@rschain.net>
2023-06-06 16:49:57 +0200

    mobile

--- components/footerResponsif.js
+++ components/footerResponsif.js
@@ -71,8 +71,8 @@ const ListHeader = ({ children }) => {
 
 export default function FooterR() {
   return (
-    <Center>
-    <Box
+    <Center w="full">
+    <Box w="full"
       bg={'#B0C4DE'}
       color={useColorModeValue('gray.700', 'gray.200')} w='full'>
       <Container as={Stack} maxW={'full'} py={10}>
