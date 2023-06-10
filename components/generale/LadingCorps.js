63aa6a84ddaebc4e0b7d18f4f8ba341ac39a79b5
KoneKD <122866866+KoneKD@users.noreply.github.com>
2023-06-10 16:23:13 +0000

    Update LadingCorps.js

--- components/generale/LadingCorps.js
+++ components/generale/LadingCorps.js
@@ -49,7 +49,7 @@ export  function ItemCard ({ item, card }) {
       <>
         {/* card  */}
         <Box height={["20vh","20vh","20vh","40vh","40vh"]}
-          width={{ base: "30%", md: "30%" }} marginBottom={{base:"5em",md:"0px"}} mb={5} borderRadius={50}>
+          width={{ base: "30%", md: "30%" }} marginBottom={{base:"5em",md:"0px"}} mb={5} mr={5} borderRadius={50}>
         <Link
           height={"15vh"}
           width={{ base: "80%", md: "30%" }}
