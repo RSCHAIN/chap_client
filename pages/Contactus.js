11e1c98639a8171115d652bf98813ef0e2fffba2
Kone Kolotioloman Dieudonne <Kolotioloman.kone@rschain.net>
2023-07-03 10:07:17 +0000

    okay

--- pages/Contactus.js
+++ pages/Contactus.js
@@ -119,7 +119,7 @@ export default function contact() {
                         <InputGroup borderColor="#E0E1E7">
                           <InputLeftElement
                             pointerEvents="none"
-                            children={<BsPerson color="gray.800" />}
+                            // children={<BsPerson color="gray.800" />}
                           />
                           <Input type="text" size="md" />
                         </InputGroup>
@@ -129,7 +129,7 @@ export default function contact() {
                         <InputGroup borderColor="#E0E1E7">
                           <InputLeftElement
                             pointerEvents="none"
-                            children={<MdOutlineEmail color="gray.800" />}
+                            // children={<MdOutlineEmail color="gray.800" />}
                           />
                           <Input type="text" size="md" />
                         </InputGroup>
