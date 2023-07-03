bfcfd42c237031d37c9dfbb16de1bf9a387a95ac
Kone Kolotioloman Dieudonne <Kolotioloman.kone@rschain.net>
2023-07-03 16:04:56 +0000

    okay again

--- pages/Contactus.js
+++ pages/Contactus.js
@@ -17,36 +17,37 @@ import {
   InputLeftElement,
   Textarea,
   useToast,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 import {
   MdPhone,
   MdEmail,
   MdLocationOn,
   MdFacebook,
   MdOutlineEmail,
-} from 'react-icons/md';
-import { BsGithub, BsDiscord, BsPerson } from 'react-icons/bs';
-import InputBar from '@/components/InputBar';
-import Navbar from '@/components/Navbar';
-import FooterR from '@/components/footerResponsif';
-import { useState } from 'react';
+} from "react-icons/md";
+import { BsGithub, BsDiscord, BsPerson } from "react-icons/bs";
+import InputBar from "@/components/InputBar";
+import Navbar from "@/components/Navbar";
+import FooterR from "@/components/footerResponsif";
+import { useState } from "react";
 
 export default function Contact() {
   const toast = useToast();
-  const [email,setEmail] = useState();
-  const [message,setMessage] = useState();
-  const [nom,setNom] = useState();
+  const [email, setEmail] = useState();
+  const [message, setMessage] = useState();
+  const [nom, setNom] = useState();
   return (
     <Container maxW="full" mt={0} centerContent overflow="hidden">
       <InputBar />
-     <Navbar></Navbar> 
-          <Flex>
+      <Navbar></Navbar>
+      <Flex>
         <Box
           bg="#02054B"
           color="white"
           borderRadius="lg"
           m={{ sm: 4, md: 16, lg: 10 }}
-          p={{ sm: 5, md: 5, lg: 16 }}>
+          p={{ sm: 5, md: 5, lg: 16 }}
+        >
           <Box p={4}>
             <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
               <WrapItem>
@@ -63,9 +64,10 @@ export default function Contact() {
                         width="200px"
                         variant="ghost"
                         color="#DCE2FF"
-                        _hover={{ border: '2px solid #1C6FEB' }}
-                        leftIcon={<MdPhone color="#1970F1" size="20px" />}>
-                        +91-988888888
+                        _hover={{ border: "2px solid #1C6FEB" }}
+                        leftIcon={<MdPhone color="#1970F1" size="20px" />}
+                      >
+                        06-05-79-90-59
                       </Button>
                       <Button
                         size="md"
@@ -73,19 +75,21 @@ export default function Contact() {
                         width="200px"
                         variant="ghost"
                         color="#DCE2FF"
-                        _hover={{ border: '2px solid #1C6FEB' }}
-                        leftIcon={<MdEmail color="#1970F1" size="20px" />}>
-                        hello@abc.com
+                        _hover={{ border: "2px solid #1C6FEB" }}
+                        leftIcon={<MdEmail color="#1970F1" size="20px" />}
+                      >
+                        srschain@gmail.com
                       </Button>
                       <Button
                         size="md"
                         height="48px"
-                        width="200px"
+                        width="250px"
                         variant="ghost"
                         color="#DCE2FF"
-                        _hover={{ border: '2px solid #1C6FEB' }}
-                        leftIcon={<MdLocationOn color="#1970F1" size="20px" />}>
-                        Karnavati, India
+                        _hover={{ border: "2px solid #1C6FEB" }}
+                        leftIcon={<MdLocationOn color="#1970F1" size="20px" />}
+                      >
+                        14 Avenue De Bourgogne,<br/> 91300 Massy
                       </Button>
                     </VStack>
                   </Box>
@@ -93,13 +97,14 @@ export default function Contact() {
                     mt={{ lg: 10, md: 10 }}
                     spacing={5}
                     px={5}
-                    alignItems="flex-start">
+                    alignItems="flex-start"
+                  >
                     <IconButton
                       aria-label="facebook"
                       variant="ghost"
                       size="lg"
                       isRound={true}
-                      _hover={{ bg: '#0D74FF' }}
+                      _hover={{ bg: "#0D74FF" }}
                       icon={<MdFacebook size="28px" />}
                     />
                     <IconButton
@@ -107,7 +112,7 @@ export default function Contact() {
                       variant="ghost"
                       size="lg"
                       isRound={true}
-                      _hover={{ bg: '#0D74FF' }}
+                      _hover={{ bg: "#0D74FF" }}
                       icon={<BsGithub size="28px" />}
                     />
                     <IconButton
@@ -115,7 +120,7 @@ export default function Contact() {
                       variant="ghost"
                       size="lg"
                       isRound={true}
-                      _hover={{ bg: '#0D74FF' }}
+                      _hover={{ bg: "#0D74FF" }}
                       icon={<BsDiscord size="28px" />}
                     />
                   </HStack>
@@ -132,7 +137,11 @@ export default function Contact() {
                             pointerEvents="none"
                             // children={<BsPerson color="gray.800" />}
                           />
-                          <Input type="text" size="md" />
+                          <Input
+                            type="text"
+                            size="md"
+                            onChange={(e) => setNom(e.target.value)}
+                          />
                         </InputGroup>
                       </FormControl>
                       <FormControl id="name">
@@ -142,7 +151,11 @@ export default function Contact() {
                             pointerEvents="none"
                             // children={<MdOutlineEmail color="gray.800" />}
                           />
-                          <Input type="text" size="md" />
+                          <Input
+                            type="text"
+                            size="md"
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
                         </InputGroup>
                       </FormControl>
                       <FormControl id="name">
@@ -150,8 +163,9 @@ export default function Contact() {
                         <Textarea
                           borderColor="gray.300"
                           _hover={{
-                            borderRadius: 'gray.300',
+                            borderRadius: "gray.300",
                           }}
+                          onChange={(e) => setMessage(e.target.value)}
                           placeholder="message"
                         />
                       </FormControl>
@@ -160,7 +174,8 @@ export default function Contact() {
                           variant="solid"
                           bg="#0D74FF"
                           color="white"
-                          _hover={{}}>
+                          _hover={{}}
+                        >
                           Envoyer
                         </Button>
                       </FormControl>
@@ -172,7 +187,7 @@ export default function Contact() {
           </Box>
         </Box>
       </Flex>
-      <FooterR/>
+      <FooterR />
     </Container>
   );
 }
