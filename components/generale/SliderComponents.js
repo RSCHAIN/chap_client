b60ba63a9835258a8d00bf924fc4849a0f91b4b1
Kone Kolotioloman Dieudonne <Kolotioloman.kone@rschain.net>
2023-07-03 10:01:19 +0000

    contact

--- components/generale/SliderComponents.js
+++ components/generale/SliderComponents.js
@@ -28,19 +28,18 @@ const SliderComponents = () => {
   const cards = [
     // "https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slide%2Ffb2.png?alt=media&token=122cce02-7a42-4e20-9978-3dc2d75e8af1",
     "https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slide%2FBakery.png?alt=media&token=574eecdf-7fc8-449f-a8d0-e5f9e14f9325",
-"https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slide%2Ffb231.png?alt=media&token=cd1f5f0e-3aef-473b-be76-b86686d398d7",
-"https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slide%2FMicrosoftTeams-image1.png?alt=media&token=58788349-42b9-4b70-ae4e-6de227c5cb04"
+    "https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slide%2Ffb231.png?alt=media&token=cd1f5f0e-3aef-473b-be76-b86686d398d7",
+    "https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slide%2FMicrosoftTeams-image1.png?alt=media&token=58788349-42b9-4b70-ae4e-6de227c5cb04",
   ];
   const [isLagerThan768] = useMediaQuery("(min-width: 768px)");
 
   return (
     <>
       <Box
+        overflow="-moz-hidden-unscrollable"
         position={"relative"}
-        height={"fit-content"}
-        width={"100%"}
-        overflow={"hidden"}
-        mb={"2em"}
+        height={{ base: "auto", md: "auto", lg: "auto" }}
+        width={{ base: "auto", md: "auto", lg: "auto" }}
       >
         {/* CSS files for react-slick */}
         <link
@@ -96,12 +95,13 @@ const SliderComponents = () => {
           {cards.map((url, index) => (
             <Box
               key={index}
-              height={{ base: "24vh", md: "56vh"}}
-              width={"80%"}
+              overflow={"auto"}
+              height={{ base: "xs", md: "xs", lg: "xl" }}
+              minw={{ base: "auto", md: "xs", lg: "xl" }}
               position="relative"
               backgroundPosition="center"
               backgroundRepeat="no-repeat"
-              backgroundSize="contain"
+              backgroundSize="cover"
               backgroundImage={url}
             ></Box>
           ))}
