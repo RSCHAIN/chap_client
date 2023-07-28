444fe76df050ac3854ed7b0934af861a0f983d02
kolo01 <konedieu5@gmail.com>
2023-07-28 19:22:30 +0200

    ooakay aoool

--- components/generale/LoginSignButton.js
+++ components/generale/LoginSignButton.js
@@ -5,18 +5,16 @@ import { useState } from "react";
 
 const LoginSignButton = () => {
     const [posta,setPosta]= useState("")
+    const [paramText,setParamText]= useState("Votre code postal")
     useEffect(()=>{
         setPosta(localStorage.getItem("postal"))
     },[posta])
     return (
         <>
         <Flex display={["none","none","none","flex","flex"]}>  
-        {posta.length>=5 ? <>
+        {posta.length<=4 ? <>  </>:<>
         
-            <Flex><Text mr={5} mt={1}>Votre code postal</Text> <Tooltip label='click, pour editer'><Editable  defaultValue={posta}>
-  <EditablePreview/>
-  <EditableInput value={posta} onChange={(e)=>{setPosta(e.target.value),localStorage.setItem("postal",e.target.value)}}/>
-</Editable></Tooltip></Flex></>:<>  <Flex><Text mr={5} mt={1}>Editer votre code postal</Text> <Tooltip label='click, pour editer'><Editable  defaultValue={posta}>
+            <Flex><Text mr={5} mt={1}>{paramText}</Text> <Tooltip label='click, pour editer'><Editable  defaultValue={posta}>
   <EditablePreview/>
   <EditableInput value={posta} onChange={(e)=>{setPosta(e.target.value),localStorage.setItem("postal",e.target.value)}}/>
 </Editable></Tooltip></Flex></>}
