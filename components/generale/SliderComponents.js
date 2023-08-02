cbcd0334691151223d5b85b0c698f694d916ed7d
kolo01 <konedieu5@gmail.com>
2023-08-02 18:52:53 +0200

    insertion de images

--- components/generale/SliderComponents.js
+++ components/generale/SliderComponents.js
@@ -38,10 +38,16 @@ const SliderComponents = () => {
   const top = useBreakpointValue({ base: "90%", md: "50%" });
   const side = useBreakpointValue({ base: "30%", md: "10px" });
   const cards = [
-    // "https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slide%2Ffb2.png?alt=media&token=122cce02-7a42-4e20-9978-3dc2d75e8af1",
     "https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slide%2FBakery.png?alt=media&token=574eecdf-7fc8-449f-a8d0-e5f9e14f9325",
     "https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slide%2FGueye%20grillade.png?alt=media&token=88ff8558-406c-4762-9462-e050349ca3e0",
     "https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slide%2FMicrosoftTeams-image1.png?alt=media&token=58788349-42b9-4b70-ae4e-6de227c5cb04",
+    "https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slider%2F3.png?alt=media&token=e5393663-2adf-4ea1-96be-a097839ec561",
+    "https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slider%2F4.png?alt=media&token=7eaa7ac6-28cb-4d7b-877f-e2014116b87a",
+    "https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slider%2F5.png?alt=media&token=51387807-493e-4457-b4e4-f4217d7fba8c",
+    "https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slider%2F7.png?alt=media&token=9938ab34-a4eb-4fa5-9527-d1e741f048c3",
+    "https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slider%2F1.png?alt=media&token=b6f83978-875d-429f-88ab-1dd2d962b49e",
+    "https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slider%2F2.png?alt=media&token=caa391bd-bffb-491f-9679-a655d3fee05f",
+    "https://firebasestorage.googleapis.com/v0/b/appchapfinal.appspot.com/o/slider%2F6.png?alt=media&token=11b0ef49-fc5e-44bd-b104-bb750053d133"
   ];
   const [isLagerThan768] = useMediaQuery("(min-width: 768px)");
 
