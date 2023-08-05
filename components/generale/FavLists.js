7aa3f7a66f96d8a184d1295b9c4346c8f2fbcbf8
kolo01 <konedieu5@gmail.com>
2023-08-05 11:35:20 +0200

    samedi 5

--- components/generale/FavLists.js
+++ components/generale/FavLists.js
@@ -116,7 +116,7 @@ export default function Favlist() {
               </Box>
               
               <Text fontWeight={"bold"} width={["100px","100px","100px","150px","150px"]} noOfLines={1} pl={2}>{data.nom}</Text>
-              <Text color={"green"} ml={"75%"} fontWeight={"semibold"} >{data.prix}€</Text>
+              <Text color={"green"} ml={"70%"} fontWeight={"semibold"} >{data.prix}€</Text>
             </Box>
             </Link>
           ))}</Carousel>
