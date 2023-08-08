e4c5bbda0cc20c45f9c59bb75bca643598e44043
kolo01 <konedieu5@gmail.com>
2023-08-08 13:22:58 +0200

    mardi 10h

--- components/generale/LadingCorps.js
+++ components/generale/LadingCorps.js
@@ -374,7 +374,7 @@ const LadingCorps = () => {
         {/* <Location /> */}
         <Center width={"100%"} height={"auto"}>
           <Box height={"95%"} width={"95%"}>
-          <Center width={"100%"}>
+          <Center width={"100%"} display={"grid","grid","grid","none","none"}>
               <Box>
                
 
