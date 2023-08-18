2bd2dc6bd206f6f6d36a794caeeb2cc799fc63d0
kolo01 <konedieu5@gmail.com>
2023-08-18 17:11:55 +0200

    fin des elements

--- pages/Connexion.js
+++ pages/Connexion.js
@@ -1,7 +1,13 @@
 "use client";
 import Navbar from "@/components/Navbar";
 import InputBar from "@/components/InputBar";
-import { AlertDescription, AlertIcon, AlertTitle, CloseButton, useMediaQuery } from "@chakra-ui/react";
+import {
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  CloseButton,
+  useMediaQuery,
+} from "@chakra-ui/react";
 import {
   Modal,
   ModalOverlay,
@@ -29,7 +35,7 @@ import {
   Heading,
   Text,
   FormHelperText,
-  FormErrorMessage
+  FormErrorMessage,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import {
@@ -48,32 +54,29 @@ export default function Connexion() {
     isOpen: isVisible,
     onClose,
     onOpen,
-  } = useDisclosure({ defaultIsOpen: true })
+  } = useDisclosure({ defaultIsOpen: true });
   const [isLagerThan768] = useMediaQuery("(min-width: 768px)");
   const [password, setPassword] = useState();
   const [email, setEmail] = useState();
-  const [verif,setVerif] = useState();
+  const [verif, setVerif] = useState();
   const auth = getAuth(app);
   const router = useRouter();
   const toast = useToast();
 
-  const getTime = () =>{
-    const currentTime = new Date()
-    const timestanp = currentTime.getTime()
-    localStorage.setItem("time",timestanp)
+  const getTime = () => {
+    const currentTime = new Date();
+    const timestanp = currentTime.getTime();
+    localStorage.setItem("time", timestanp);
     // console.log("okay")
-  }
-
-
+  };
 
   const loginUSer = async () => {
     await signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        getTime()
-        if(userCredential.user.emailVerified){
-
+        getTime();
+        if (userCredential.user.emailVerified) {
           setEmail(userCredential.user.email);
-          sessionStorage.setItem("email",userCredential.user.email)
+          sessionStorage.setItem("email", userCredential.user.email);
           // router.back()
           toast({
             title: "ACCES AUTORISE.",
@@ -83,9 +86,9 @@ export default function Connexion() {
             isClosable: true,
           });
           router.back();
-        }else{
+        } else {
           // sendEmailVerification(auth.currentUser)
-          signOut(auth)
+          signOut(auth);
           // toast({
           //   title: "Email Non Verifié.",
           //   description: "Verifier Vos Mails Afin De Confirmer La Transaction",
@@ -93,11 +96,10 @@ export default function Connexion() {
           //   duration: 200000,
           //   // isClosable: true,
           // });
-          setVerif(true)
+          setVerif(true);
           // setTimeout( router.reload(), "10000")
           // router.reload()
         }
-       
       })
       .catch((error) => {
         // throw error;
@@ -114,8 +116,9 @@ export default function Connexion() {
             duration: 9000,
             isClosable: true,
           });
-        }
-        else if (errorMessage == "Firebase: Error (auth/too-many-requests).") {
+        } else if (
+          errorMessage == "Firebase: Error (auth/too-many-requests)."
+        ) {
           toast({
             title: "TROP DE TENTATIVES.",
             description: "VEUILLEZ REESAYER PLUS TARD",
@@ -123,9 +126,17 @@ export default function Connexion() {
             duration: 9000,
             isClosable: true,
           });
-         }
-          else {
-            if (errorMessage == "Firebase: Error (auth/wrong-password).") {
+        } else {
+          if (errorMessage == "Firebase: Error (auth/wrong-password).") {
+            toast({
+              title: "MOT DE PASSE/IDENTIFIANT INCORRECT",
+              description: "VEUILLEZ VERIFIER VOS ACCES",
+              status: "error",
+              duration: 9000,
+              isClosable: true,
+            });
+          } else {
+            if (errorMessage == "Firebase: Error (auth/invalid-email).") {
               toast({
                 title: "MOT DE PASSE/IDENTIFIANT INCORRECT",
                 description: "VEUILLEZ VERIFIER VOS ACCES",
@@ -133,126 +144,137 @@ export default function Connexion() {
                 duration: 9000,
                 isClosable: true,
               });
-            }else{
-              if (errorMessage == "Firebase: Error (auth/invalid-email).") {
-                toast({
-                  title: "MOT DE PASSE/IDENTIFIANT INCORRECT",
-                  description: "VEUILLEZ VERIFIER VOS ACCES",
-                  status: "error",
-                  duration: 9000,
-                  isClosable: true,
-                });
-              }else{
-                // console.log(error);
-              };
+            } else {
+              // console.log(error);
             }
-         }
-      
+          }
+        }
       });
   };
-  const isError = email === ''
+  const isError = email === "";
 
   return (
     <>
       <InputBar />
       {isLagerThan768 ? <Navbar></Navbar> : <></>}
       {/* <Navbar /> */}
-    
+
       <Center display={"grid"}>
-      {verif ? ( <Alert status='error' w={'fit-content'}>
-      <AlertIcon />
-      <Box>
-        <AlertTitle>Mail Pas Verifié!</AlertTitle>
-        <AlertDescription>
-         <p> Merci de bien vouloir consulter vos   mails afin de confirmer votre inscription sur notre site </p>
-       
-        </AlertDescription>
-      </Box>
-      <CloseButton
-        alignSelf='flex-start'
-        position='relative'
-        right={-1}
-        top={-1}
-        onClick={onClose}
-      />
-    </Alert>
-  ) : (<></>
-   // <Button onClick={onOpen}>Show Alert</Button>
-  )}
+        {verif ? (
+          <Alert status="error" w={"fit-content"}>
+            <AlertIcon />
+            <Box>
+              <AlertTitle>Mail Pas Verifié!</AlertTitle>
+              <AlertDescription>
+                <p>
+                  {" "}
+                  Merci de bien vouloir consulter vos mails afin de confirmer
+                  votre inscription sur notre site{" "}
+                </p>
+              </AlertDescription>
+            </Box>
+            <CloseButton
+              alignSelf="flex-start"
+              position="relative"
+              right={-1}
+              top={-1}
+              onClick={onClose}
+            />
+          </Alert>
+        ) : (
+          <></>
+          // <Button onClick={onOpen}>Show Alert</Button>
+        )}
         <Flex
-        textAlign={"center"}
-          bgColor={["white","white","white","white","white"]}
+          textAlign={"center"}
+          bgColor={["white", "white", "white", "white", "white"]}
           borderRadius={5}
-         
           w={"90%"}
           h={500}
           mx={[5, 5, 5, 5, 12]}
           my={5}
           // boxShadow={"0px 4px 24px "}
-  
         >
           <Box h={500}>
             <Image
               height={500}
-              alt={'logo'}
+              alt={"logo"}
               width={650}
               src="/logo1.png"
               display={["none", "none", "flex", "flex", "flex"]}
             />
           </Box>
           <Center>
-            <Box width={"full"} color={"black"} >
-              
+            <Box width={"full"} color={"black"}>
               <FormControl isInvalid={isError}>
-              <Stack spacing={4}>
-                <Heading display={["none","none","none","grid","grid"]} ml={["10%","10%","10%","0%","0%"]}>Bienvenue</Heading>
-                <Text display={["none","none","none","grid","grid"]}>Connectez-vous á votre compte</Text>
-                <Input
-                  type={"text"}
-                  placeholder="Email"
-                  border={"2px solid gray"}
-                  // borderRadius={"50px"}
-                  width={["200px", "200px", "350px", "350px", "350px"]}
-                  onChange={(ev) => setEmail(ev.target.value.trim().toLowerCase())}
-                  color={"gray.500"}
-                />
-                <Input
-                  type={"password"}
-                  placeholder={"Mot de passe"}
-                  border={"2px solid gray"}
-                  // borderRadius={"50px"}
-                  onChange={(e) => setPassword(e.target.value)}
-                  width={["200px", "200px", "350px", "350px", "350px"]}
-                />
-                <TransitionExample/>
-                <Center>
-                <Button
-                width={"fit-content"}
-                  // borderRadius={"50px"}
-                  bgColor={"#08566e"}
-                  color={"white"}
-                  _hover={{
-                    bg: '#08566e',
-                  }}
-                  onClick={() => loginUSer()}
-                >
-                  Connexion{" "}
-                </Button>
-                <Link width={"fit-content"} mt={20} fontSize={20} textAlign={'center'} height={"40px"} border={"1px solid #08566e"} borderRadius={"5%"}   _hover={{
-                color: '#C0C0C0',
-              }} href={'/Inscription'}>
-                  Inscription
-                </Link>
-                </Center>
-              </Stack>
-      {!isError ? (
-        <FormHelperText>
-         
-        </FormHelperText>
-      ) : (
-        <FormErrorMessage>Email is required.</FormErrorMessage>
-      )}
-    </FormControl>
+                <Stack spacing={4}>
+                  <Heading
+                    display={["none", "none", "none", "grid", "grid"]}
+                    ml={["10%", "10%", "10%", "0%", "0%"]}
+                  >
+                    Bienvenue
+                  </Heading>
+                  <Text display={["none", "none", "none", "grid", "grid"]}>
+                    Connectez-vous á votre compte
+                  </Text>
+                  <Input
+                    type={"text"}
+                    placeholder="Email"
+                    border={"2px solid gray"}
+                    // borderRadius={"50px"}
+                    width={["200px", "200px", "350px", "350px", "350px"]}
+                    onChange={(ev) =>
+                      setEmail(ev.target.value.trim().toLowerCase())
+                    }
+                    color={"gray.500"}
+                  />
+                  <Input
+                    type={"password"}
+                    placeholder={"Mot de passe"}
+                    border={"2px solid gray"}
+                    // borderRadius={"50px"}
+                    onChange={(e) => setPassword(e.target.value)}
+                    width={["200px", "200px", "350px", "350px", "350px"]}
+                  />
+                  <TransitionExample />
+                  <Center>
+                    <Button
+                      width={"fit-content"}
+                      // borderRadius={"50px"}
+                      bgColor={"#08566e"}
+                      color={"white"}
+                      _hover={{
+                        bg: "#08566e",
+                      }}
+                      onClick={() => loginUSer()}
+                    >
+                      Connexion{" "}
+                    </Button>
+                    <Box>
+                    <Link
+                      width={"fit-content"}
+                      mt={10}
+                      fontSize={20}
+                      // textAlign={"center"}
+                      height={"40px"}
+                      border={"1px solid #08566e"}
+                      borderRadius={"5%"}
+                      _hover={{
+                        color: "#C0C0C0",
+                      }}
+                      href={"/Inscription"}
+                    >
+                      Inscription
+                    </Link>
+                    </Box>
+                  </Center>
+                </Stack>
+                {!isError ? (
+                  <FormHelperText></FormHelperText>
+                ) : (
+                  <FormErrorMessage>Email is required.</FormErrorMessage>
+                )}
+              </FormControl>
             </Box>
           </Center>
         </Flex>
