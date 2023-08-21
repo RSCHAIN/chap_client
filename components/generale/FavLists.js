c3499b80f3009b1972686bd192f2bf660a12a12a
kolo01 <konedieu5@gmail.com>
2023-08-21 17:17:48 +0200

    bg remove code postal

--- components/generale/FavLists.js
+++ components/generale/FavLists.js
@@ -76,39 +76,6 @@ export default function Favlist() {
   },[check])
   const [slider, setSlider] = useState(null);
   return <>
-  {/* <SimpleGrid columns={[2,2,2,4,5]} spacing={2}>
-     {/* CSS files for react-slick */}
-     {/* <link
-          rel="stylesheet"
-          type="text/css"
-          charSet="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        />
-    {Object.values(data).splice(0,5).map((data,index)=>(
-      <Box
-      key={index} my={10}bgColor={"#eee2"} borderRadius={"25px"} pb={5} boxShadow={"grey 1px 1px  5px"} width={"200px"}>
-        <Box
-     
-        borderRadius={"25px 25px 0px 0px"}
-        width={"full"} 
-        height={"100px"}
-        bgImage={data.imageUrl}
-        backgroundPosition={"center"}
-        backgroundSize={"cover"}
-        backgroundRepeat={"no-repeat"}
-        >
-      
-        </Box>
-        
-        <Text fontWeight={"bold"}>{data.nom}</Text>
-      </Box>
-    ))}
-    </SimpleGrid> */}
     <Box ml={[5,5,5,10,10]} width={"90%"}>
     <Carousel  responsive={responsive} style={"marginLeft='10px'"}
   >
@@ -122,26 +89,12 @@ export default function Favlist() {
             <Box
             
             key={index} my={10}bgColor={"#eee2"} pb={5} boxShadow={"grey 1px 1px  5px"} width={["150px","150px","150px","150px","150px"]} borderRadius={"15px"}>
-              {/* <Box
-             
-              borderRadius={"25px 25px 0px 0px"}
-              width={"full"} 
-              height={"100px"}
-              bgImage={data.imageUrl}
-              backgroundPosition={"center"}
-              backgroundSize={"cover"}
-              
-              backgroundRepeat={"no-repeat"}
-              >
-               
-            
-              </Box> */}
               <Image height={["100px","100px","100px","100px","100px"]}  width={["150px","150px","150px","150px","150px"]} src={data.imageUrl}  alt={data.nom} borderRadius={"15px 15px 0px 0px"} />
 
-              <Text fontWeight={"bold"} width={"fit-content"} noOfLines={1} pl={2} fontSize={"20px"}>{data.nom}</Text>
+              <Text fontWeight={"bold"} width={"fit-content"} noOfLines={1} pl={2} fontSize={["15px","15px","15px","20px","20px"]}>{data.nom}</Text>
               <Flex justifyContent={"space-between"}>
-              <Text fontWeight={"bold"} width={"fit-content"} color={"orange.900"}  pl={2} fontSize={"15px"}>{data.organisation}</Text>
-              <Text color={"black"}  fontWeight={"bold"} fontSize={"15px"}>{data.prix}€</Text>
+              <Text fontWeight={"bold"} width={"fit-content"} color={"orange.900"}  pl={2} fontSize={["13px","13px","13px","15px","15px"]}>{data.organisation}</Text>
+              <Text color={"black"}  fontWeight={"bold"} fontSize={["13px","13px","13px","15px","15px"]}>{data.prix}€</Text>
               </Flex>
              
             </Box>
