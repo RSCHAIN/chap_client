77fd2068dd10332e053c7cffeddcb15e8af1258b
kolo01 <konedieu5@gmail.com>
2023-08-21 18:12:44 +0200

    banniere web

--- components/generale/LadingCorps.js
+++ components/generale/LadingCorps.js
@@ -389,7 +389,7 @@ const LadingCorps = () => {
             <Flex width={"100%"} height={"15vh"} bgColor={"#08566e"} display={["none","none","none","flex","flex"]} mt={-3} color={"white"}>
               <Center width={"100%"}>
                 <Box  display={"flex"}>
-                  <Box ml={10} textAlign={"center"}>
+                  <Box mr={10} textAlign={"center"}>
                     <Text fontSize={"25px"} width={"full"} _hover={{
                       bgColor:"black",
                       opacity:"0.7"
