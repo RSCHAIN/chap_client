c51d366bdab97e39b92ae7dd430bcfc6517d3333
kolo01 <konedieu5@gmail.com>
2023-09-04 11:56:42 +0200

    okay devis

--- pages/Devis.js
+++ pages/Devis.js
@@ -1,7 +1,9 @@
-import { Box,Flex,Center,Heading,Text,Image, Input,Button,InputGroup,InputRightElement, Select, SimpleGrid } from "@chakra-ui/react";
+import { Box,Flex,Center,Heading,Text,Image, Input,Button,InputGroup,InputRightElement, Select, SimpleGrid, useToast } from "@chakra-ui/react";
 import InputBar from "@/components/InputBar";
 import Navbar from "@/components/Navbar";
 import {useState} from "react"
+import { addDoc, collection, doc, getFirestore, setDoc } from "firebase/firestore";
+import { app, db } from "@/FIREBASE/clientApp";
 
 export default function Devis(){
   const [email,setEmail] = useState("")
@@ -16,6 +18,34 @@ export default function Devis(){
   const [hauteur,setHauteur] = useState("")
   const [details,setDetails] = useState("")
   const [valeur,setValeur] = useState("")
+  const firestore = getFirestore(app);
+  const toast = useToast()
+  const makeDevis = async  ()=>{
+    await addDoc(collection(db, "DevisPerso"), {
+      email,
+      numero,
+      dest,
+      codeDest,
+      arriv,
+      codeArriv,
+      poids,
+      longueur,
+      largeur,
+      hauteur,
+      details,
+      valeur
+    }).then(()=>{ toast({
+      title: "Devis envoyé",
+      description: "Nous vous contacterons!!",
+      status: "success",
+      duration: 10000,
+      isClosable: true,
+    })});
+   
+    
+  }
+
+
   const CI = "Côte d'Ivoire"
   const slog="Comparer différents transporteur et faites des économies avec CHAP  "
   const slog1 = "Envoyez vos colis vers vers l'Afrique de l'ouest par nos partenaires. Remplissez le formulaire et attendez notre retour mail si vous ne possédez pas un CHAP, sinon vérifiez l'onglet devis de votre compte. Un fois reçu les différentes propositions de nos partenaires, vous pourrez choisir, payer et préparer le colis pour le transporteur."
@@ -106,12 +136,12 @@ export default function Devis(){
               </Flex>
             </Box>
             <Button width={"100%"} bgColor={"cyan.900"} color={"white"}
-            
+            onClick={()=>makeDevis()}
             >Envoyer dès maintenant</Button>
           </Box>
 
         </Box>
-        <Box width={["100%","100%","100%","40%","40%"]}>
+        <Box width={["100%","100%","100%","50%","50%"]}>
         <Image src='./pic2.jpg' alt="image livraison" />
           <Heading fontSize={"20px"} textAlign={["right","right","right","left","left"]}>{slog}</Heading>
        
