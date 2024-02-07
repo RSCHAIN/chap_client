d19bc3f54c8348f6737ca6a63ed73c5fcc8afe25
kolo01 <konedieu5@gmail.com>
2024-02-07 10:12:18 +0000

    24 octobre

---
+++ components/Categorie/boxed/BoxRestau.js
@@ -0,0 +1,238 @@
+import { ChevronDownIcon, ChevronUpIcon, StarIcon } from '@chakra-ui/icons';
+import { Box, Collapse, Flex, Heading, Image, Text,Link, Center, Button } from '@chakra-ui/react';
+import React,{useState} from 'react'
+import { StarM } from '../Restaurant';
+import { MdFastfood, MdIosShare, MdLocationOn } from 'react-icons/md';
+import { TfiWorld } from 'react-icons/tfi';
+import { BsFillTelephoneOutboundFill } from 'react-icons/bs';
+import ReservationButton from '@/components/ReservationButton';
+import ReservationCoiff from '@/components/ReservationCoiffure';
+import Mapped from '../Map';
+import { FaEdit } from 'react-icons/fa';
+import { BsCashCoin } from "react-icons/bs";
+
+function BoxRestau({
+    mag,categorie
+}) {
+    const day = [
+        "lundi",
+        "mardi",
+        "mercredi",
+        "jeudi",
+        "vendredi",
+        "samedi",
+        "dimanche",
+      ];
+    
+    const [show, setShow] = useState(false);
+    const handleToggle = () => setShow(!show);
+  
+
+
+
+  return (
+    <>
+      <Image
+        src={mag.imageUrl}
+        display={"block"}
+        alt={`logo de ${mag.organisation}`}
+        width={"100%"}
+        maxHeight={"70vh"}
+        fit={"cover"}
+        bgSize={"cover"}
+
+      />
+       <Center display={"flex"} bgColor={"white"} pt={5}>
+      
+          
+      <Heading mr={"60%"} width={"fit-content"}>{mag.organisation}</Heading>
+      <Flex >
+          <Button bgColor={"transparent"} _hover={{
+              bgColor:'transparent2'
+          }} leftIcon={<FaEdit />}>Avis</Button>
+            <Button bgColor={"transparent"} _hover={{
+              bgColor:'transparent2'
+          }} leftIcon={<MdIosShare />}>Partager</Button>
+      </Flex>
+
+</Center>
+      <Flex bgColor={"white"} justifyContent={"space-between"}>
+      <Box width={"80%"} bgColor={"white"} mb={10} display={"flex"} justifyContent={"center"} flexDirection={"column" } padding={{ base: 5, lg: 7 }}>
+       
+      
+        <Center> 
+        <Flex mr={"47%"} mt={2}>
+            <MdLocationOn color='red' fontSize={25}/>
+            <Text>{mag.adresse}</Text>
+        </Flex>
+        <Flex></Flex>
+        </Center>
+        <Center>
+        <Flex mr={"32%"} mt={2}>
+            <Box mt={2}>
+            <StarM data={mag.feedback} />
+            </Box>
+        
+        <Flex mb={2}  mt={2} as={Link} href={`tel:${mag.number}`}>
+              <BsFillTelephoneOutboundFill fontSize={20} />
+              <Text
+
+                fontSize={"15px"}
+                fontWeight={"medium"}
+                ml={2}
+                color={"green"}
+              >
+                {mag.number}
+              </Text>
+            </Flex>
+            <Flex mb={2}  mt={2}  ml={5} as={Link} href={`tel:${mag.number}`}>
+              <MdFastfood fontSize={20} />
+              <Text
+
+                fontSize={"15px"}
+                fontWeight={"medium"}
+                ml={2}
+                color={"green"}
+              >
+                Carte
+              </Text>
+            </Flex>      
+
+            {mag.siteWeb ?
+              <Flex mb={2} cursor={"pointer"} display={{ base: "flex", lg: "flex" }} mt={2}   ml={5} as={Link} isExternal href={`${mag.siteWeb}`} >
+                <TfiWorld fontSize={20} />
+                <Text
+
+                  fontSize={"15px"}
+                  fontWeight={"medium"}
+                  ml={1}
+                  width={'70px'}
+                  color={"green"}
+                >
+                  Site web
+                </Text>
+              </Flex> : <Flex display={{ base: "flex", lg: "flex" }} mb={2} mt={2} >
+                <TfiWorld fontSize={20} />
+                <Text
+                  fontSize={"15px"}
+
+                  fontWeight={"medium"}
+                  ml={2}
+                  color={"green"}
+                >
+                  Non defini
+                </Text>
+              </Flex>
+            }
+
+<Flex width={"100%"}  mt={2}>
+<BsCashCoin fontSize={20} />
+            <Box display={ "flex"} ml={2} mt={-1} width={"full"} >
+              {mag.methodeDePaiement != "undefined" &&
+                mag.methodeDePaiement != null
+                ? Object.values(mag.methodeDePaiement).map(methPaiement => {
+                  return (
+                    <Text key={methPaiement} >
+                      {methPaiement},
+                    </Text>
+                  );
+                })
+                : "Espèces"}
+            </Box>
+          </Flex>
+          </Flex>
+        </Center>
+        <Center>
+        <Flex mr={"43%"}>
+            <Text fontWeight={"bold"} pr={2}>
+              Description :{" "}
+            </Text>
+            {mag.description == "undefined" ? (
+              <Text width={"58%"} maxWidth={"58%"} textAlign={"justify"}>
+                {`${mag.categorie} Africain`}
+              </Text>
+            ) : (
+              <Text width={"58%"} maxWidth={"58%"} textAlign={"justify"}>
+                {mag.description}
+              </Text>
+            )}
+          </Flex>
+        </Center>
+        <Center justifyContent={"space-around"}>
+        <Flex>
+            <Text fontWeight={"bold"}>Nationalité : </Text>
+            {mag.nationalite == "undefined" ? (
+              <Text ml={2} fontSize={"15px"}>
+                Africaine
+              </Text>
+            ) : (
+              <Text ml={2} fontSize={"15px"}>
+                {`${(" ", mag.nationalite)} `}
+              </Text>
+            )}
+          </Flex>
+          <Flex>
+            <Text fontWeight={"bold"}>Reservation : </Text>
+            <Text  textAlign={"justify"}>
+              {(mag.categorie != "Cosmetique" ||
+                mag.categorie != "Epicerie" ||
+                mag.categorie != "Commerce de meches" ||
+                mag.categorie != "Fret" ||
+                mag.categorie != "Salon de Coiffure") != 0 ? (
+                <Text color={"messenger.500"} ml={2}>
+                  En ligne
+                </Text>
+              ) : (
+                <Text color={"red.400"} ml={2}>
+                  Non Disponible
+                </Text>
+              )}
+            </Text>
+          </Flex>
+          <Flex >
+          <Heading
+            as={"h3"}
+            fontWeight={"bold"}
+            _hover={{
+              cursor: "pointer",
+            }}
+            onClick={handleToggle}
+            color={"blue.700"}
+            fontSize={"15px"}
+          
+          >
+            {"Horaire"}
+            {show ? (
+              <ChevronUpIcon fontSize={"20px"} />
+            ) : (
+              <ChevronDownIcon fontSize={"20px"} />
+            )}{" "}
+            :
+          </Heading>
+          <Collapse in={show}>
+            <Box ml={10}>
+              {day.map((data, index) => (
+                <Text key={index} fontSize={"15px"}>
+                  {data}:
+                  {mag.horaire != undefined && mag.horaire != null
+                    ? `${" "} ${mag.horaire[index] ? mag.horaire[index] : "Non Renseigné"}`
+                    : " Non Renseigné"}
+                </Text>
+              ))}
+            </Box>
+          </Collapse>
+          </Flex>
+        </Center>
+        <Center>
+        {categorie == "Restaurant" ? <ReservationButton mag={mag.organisation} adresse={mag.adresse} imageMag={mag.imageUrl} /> : categorie == "Salon de Coiffure" ? <ReservationCoiff mag={mag.organisation} adresse={mag.adresse} imageMag={mag.imageUrl} /> : <></>}
+        </Center>
+      </Box>
+      <Box  bgColor={"white"} mt={10} mr={50} >
+          <Mapped adresse={mag.adresse} numero={mag.number} web={mag.siteWeb} />
+        </Box>
+        </Flex>
+    </>
+  )
+}
+
+export default BoxRestau
