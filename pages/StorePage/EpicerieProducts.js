a675a4f155dfe03df325e9143fd2e0cf37c4f8dd
kolo01 <konedieu5@gmail,com>
2024-04-22 10:52:53 +0000

    modification de storepage/epicerieProducts

--- pages/StorePage/EpicerieProducts.js
+++ pages/StorePage/EpicerieProducts.js
@@ -156,7 +156,7 @@ function EpicerieProducts() {
                                     <Link href={`/Details/details?c=${"Epicerie"}&m=${order.organisation}&p=${Object.keys(epicerieDetails)[index]}`} key={index} className="">
                                         <div className="bg-white p-4 flex flex-col mb-10 items-center relative">
                                             <div className="w-fullrounded-full mb-2">
-                                                <img className="w-full h-[10rem]" src={order.imageUrl} alt="" />
+                                                <img className="w-full h-[6rem] lg:h-[10rem]" src={order.imageUrl} alt="" />
                                             </div>
                                             <h3 className="uppercase font-bold mb-2 text-xs lg:text-sm">{order.nom}</h3>
                                             <small className="bg-green-600 p-1 rounded-2xl capitalize text-white absolute -top-2 left-0">{order.etat}</small>
@@ -174,7 +174,7 @@ function EpicerieProducts() {
                                                 <span className="text-slate-700 text-sm mb-2"><FontAwesomeIcon className="mr-2" icon={faMoneyBillTransfer} />Payez en espèce</span>
                                             </div>
                                             <span className="self-end mb-2 text-xl lg:text-2xl text-red-600 font-bold">{order.prix}€</span>
-                                            <div className="w-full mt-4 flex justify-between">
+                                            <div className="w-full mt-4 flex flex-col gap-2 lg:gap-0 lg:flex-row justify-between">
                                                 <Link href={`/otherContent/intermed1?categorie=${"Epicerie"}&magasin=${order.organisation}`} className="text-white font-bold bg-amber-800 text-xs lg:text-[1rem] py-2 px-4 rounded-3xl">Commerce</Link>
                                                 <button className="text-white font-bold bg-cyan-800 text-xs lg:text-[1rem] py-2 px-4 rounded-3xl" onClick={()=>AddToCart(order, Object.keys(epicerieDetails)[index])}>+Ajouter</button>
                                             </div>
